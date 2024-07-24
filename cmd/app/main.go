2efa7ff34992d79f2fb8f58fa727af930594637b
Alexander Poole <alex.o.poole@gmail.com>
2024-07-24 13:34:03 +0200

    Add libraries needed for crud

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,4 +1,72 @@
 package main
 
-func main() {}
+import (
+	"net/http"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/jmoiron/sqlx"
+	"log"
+)
+
+var schema = `
+CREATE TABLE person (
+    first_name text,
+    last_name text,
+    email text
+)`
+
+type Person struct {
+	FirstName string `db:"first_name"`
+	LastName  string `db:"last_name"`
+	Email     string
+}
+
+func main() {
+	url := "postgres://alexanderp:magic@localhost:5432/alexanderp"
+	// this connects & tries a simple 'SELECT 1', panics on error
+	// use sqlx.Open() for sql.Open() semantics
+	db, err := sqlx.Connect("pgx", url)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// exec the schema or fail; multi-statement Exec behavior varies between
+	// database drivers;  pq will exec them all, sqlite3 won't, ymmv
+	db.MustExec("DROP TABLE person;")
+	db.MustExec(schema)
+
+	tx := db.MustBegin()
+	tx.MustExec("INSERT INTO person (first_name, last_name, email) VALUES ($1, $2, $3)", "Jason", "Moiron", "jmoiron@jmoiron.net")
+	tx.MustExec("INSERT INTO person (first_name, last_name, email) VALUES ($1, $2, $3)", "John", "Doe", "johndoeDNE@gmail.net")
+	tx.Commit()
+
+	// Selects Mr. Smith from the database
+	rows, err := db.NamedQuery(`SELECT * FROM person WHERE first_name=:fn`, map[string]interface{}{"fn": "John"})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	/*var persons []Person
+	err = rows.StructScan(&persons)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(persons)*/
+	log.Printf("Rows: %v\n", rows)
+
+	var persons []Person
+	err = db.Select(&persons, "select * from person")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Persons: %v\n", persons)
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome"))
+	})
+	http.ListenAndServe(":3000", r)
+
+}
--- go.mod
+++ go.mod
@@ -3,36 +3,17 @@ module github.com/owodunni/hano-scraper
 go 1.22
 
 require (
-	github.com/breml/rootcerts v0.2.16
 	github.com/go-chi/chi/v5 v5.0.12
-	github.com/golang/mock v1.6.0
-	github.com/lib/pq v1.10.9
-	github.com/prometheus/client_golang v1.19.0
-	github.com/qdm12/goservices v0.1.0
-	github.com/qdm12/gosettings v0.4.1
-	github.com/qdm12/gosplash v0.1.0
-	github.com/qdm12/gotree v0.2.0
-	github.com/qdm12/log v0.1.0
-	github.com/stretchr/testify v1.9.0
-	go.uber.org/mock v0.4.0
+	github.com/jackc/pgx/v5 v5.6.0
+	github.com/jmoiron/sqlx v1.4.0
 )
 
 require (
-	github.com/beorn7/perks v1.0.1 // indirect
-	github.com/cespare/xxhash/v2 v2.2.0 // indirect
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/fatih/color v1.16.0 // indirect
-	github.com/kr/text v0.2.0 // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/prometheus/client_model v0.6.0 // indirect
-	github.com/prometheus/common v0.51.1 // indirect
-	github.com/prometheus/procfs v0.13.0 // indirect
-	golang.org/x/exp v0.0.0-20240325151524-a685a6edb6d8 // indirect
-	golang.org/x/sys v0.18.0 // indirect
-	google.golang.org/protobuf v1.33.0 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
-	kernel.org/pub/linux/libs/security/libcap/cap v1.2.69 // indirect
-	kernel.org/pub/linux/libs/security/libcap/psx v1.2.69 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
+	golang.org/x/crypto v0.17.0 // indirect
+	golang.org/x/sync v0.6.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 )
